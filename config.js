03a58296e3cb1eacaf214664ea7284090167b9cc
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2023-12-29 17:51:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsPiZk9yOnVvce9eUjhDSMnty14HdFeRE",
+  authDomain: "app-mais-util-de-todos.firebaseapp.com",
+  projectId: "app-mais-util-de-todos",
+  storageBucket: "app-mais-util-de-todos.appspot.com",
+  messagingSenderId: "492183972327",
+  appId: "1:492183972327:web:97cd465130df8b6ee93542",
+  measurementId: "G-TRS055YCRJ"
+};
